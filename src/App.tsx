cc573553f0837950a4c59a793b5173c91c1e76ec
MIguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2024-01-25 12:27:06 -0600

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className='App'>
+      <h1
+        className='bold text-blue-400'
+      >
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
